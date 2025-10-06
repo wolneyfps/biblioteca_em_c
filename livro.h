ea09513b3e0ab4e3ee88235e7086b777c632a286
Wolney <gwolneyd@gmail.com>
2025-10-06 18:14:14 -0300

    feat: Define a estrutura de dados para Livro

---
+++ livro.h
@@ -0,0 +1,14 @@
+//
+// Created by gwoln on 06/10/2025.
+//
+
+#ifndef BIBLIOTECA_EM_C_LIVRO_H
+#define BIBLIOTECA_EM_C_LIVRO_H
+typedef struct {
+    int id;
+    char titulo[100];
+    char autor[100];
+    int anoPublicacao;
+    int quantidade;
+} Livro;
+#endif //BIBLIOTECA_EM_C_LIVRO_H
