af30fed356cb121742b17dc122e4a5b3138b9a7f
Wolney <gwolneyd@gmail.com>
2025-10-06 22:02:00 -0300

    feat: Define a estrutura de dados e protótipos para Empréstimo

---
+++ emprestimo.h
@@ -0,0 +1,14 @@
+#ifndef BIBLIOTECA_EM_C_EMPRESTIMO_H
+#define BIBLIOTECA_EM_C_EMPRESTIMO_H
+#include <time.h>
+
+typedef struct {
+    int id_livro;
+    int id_usuario;
+    time_t data_emprestimo;
+} emprestimo;
+
+void realizaremprestimo(void);
+
+void devolverlivro(void);
+#endif //BIBLIOTECA_EM_C_EMPRESTIMO_H
